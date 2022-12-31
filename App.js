f56b940b4d25cbea946393b2087a23b324a7c8e8
Akshay Sharma <akshay@punchh.com>
2022-12-31 12:41:37 +0530

    Removed

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement('h1', { style: { fontSize: '50px', color: 'red' } }, 'Namaste React');
+const heading2 = React.createElement('h2', { style: {} }, 'Ram-Ram React');
+
+const container = React.createElement('div', {}, [heading, heading2]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(container);
