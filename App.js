6518bc29222904e7692eb52ae33436b398d89be8
Akshay Sharma <akshay@punchh.com>
2023-01-01 12:12:58 +0530

    - JSX

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement('h1', { style: { fontSize: '50px', color: 'red' } }, 'Namaste React');
+const heading = <h1>Namaste React</h1>;
 const heading2 = React.createElement('h2', { style: {} }, 'Ram-Ram React');
 
 const container = React.createElement('div', {}, [heading, heading2]);
