8df7bb7f03a49707ead25716569d650c1ae685a1
Akshay Sharma <akshay@punchh.com>
2023-01-01 10:57:29 +0530

    npx parcel index.html
    type=module as React can’t understand import

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const heading = React.createElement('h1', { style: { fontSize: '50px', color: 'red' } }, 'Namaste React');
 const heading2 = React.createElement('h2', { style: {} }, 'Ram-Ram React');
 
